3fe8a67a1404a49a6f0e3fec314c657b0713b4b9
simoneliskases <simon.eliskases@outlook.de>
2021-04-26 14:05:01 +0200

    Added conflict4 branch

--- Assets/MyGame/Scripts/ConflictScript.cs
+++ Assets/MyGame/Scripts/ConflictScript.cs
@@ -7,7 +7,7 @@ public class ConflictScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("conflict4");   
     }
 
     // Update is called once per frame
