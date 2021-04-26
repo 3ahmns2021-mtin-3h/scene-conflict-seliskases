81566c28336bf0328e07bca13c4bf2d112f624ea
simoneliskases <simon.eliskases@outlook.de>
2021-04-26 14:05:47 +0200

    Updated Main

--- Assets/MyGame/Scripts/ConflictScript.cs
+++ Assets/MyGame/Scripts/ConflictScript.cs
@@ -7,7 +7,7 @@ public class ConflictScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("main");
     }
 
     // Update is called once per frame
